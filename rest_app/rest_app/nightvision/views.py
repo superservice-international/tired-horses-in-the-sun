8e57773ada95e5f33b6578de2b20041410eb9860
Christoph Reimers <christoph@superservice-international.com>
2015-03-29 21:55:22 +0200

    added permission

--- rest_app/rest_app/nightvision/views.py
+++ rest_app/rest_app/nightvision/views.py
@@ -1,13 +1,16 @@
 from .models import Picture
 from .serializers import PictureSerializer
 from rest_framework import generics
+from rest_framework import permissions
 
 
 class PictureList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Picture.objects.all()
     serializer_class = PictureSerializer
 
 
 class PictureDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Picture.objects.all()
     serializer_class = PictureSerializer
--- rest_app/rest_app/rest_app/urls.py
+++ rest_app/rest_app/rest_app/urls.py
@@ -6,13 +6,15 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'rest_app.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('nightvision.urls')),
 )
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
 
 if settings.DEBUG:
     urlpatterns += patterns('',
