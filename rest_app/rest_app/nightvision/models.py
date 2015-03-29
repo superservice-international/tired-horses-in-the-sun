fe2f8766b5c977dc14defa4e59ec7465af92eb01
Christoph Reimers <christoph@superservice-international.com>
2015-03-29 22:53:57 +0200

    ordering

--- rest_app/rest_app/nightvision/models.py
+++ rest_app/rest_app/nightvision/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Picture(models.Model):
 
     class Meta:
-        ordering = ('created',)
+        ordering = ('-created',)
 
     created = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField()
